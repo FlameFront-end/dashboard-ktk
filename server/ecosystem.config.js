3228d71da59381df7d0f428b2385ddea3bc316e6
FlameFront-end <5017_30@mail.ru>
2025-05-14 20:43:00 +0300

    fix: build

---
+++ server/ecosystem.config.js
@@ -0,0 +1,25 @@
+// ecosystem.config.js
+require('dotenv').config({ path: './env.production' }); // Загружаем переменные из файла env.production
+
+module.exports = {
+  apps: [
+    {
+      name: 'dashboard-api',
+      script: 'dist/main.js',
+      env: {
+        NODE_ENV: 'production',
+        PORT: process.env.PORT,
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PORT: process.env.POSTGRES_PORT,
+        POSTGRES_USER: process.env.POSTGRES_USER,
+        POSTGRES_DB: process.env.POSTGRES_DB,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        JWT_SECRET: process.env.JWT_SECRET,
+        SMTP_USER: process.env.SMTP_USER,
+        SMTP_PASS: process.env.SMTP_PASS,
+        TELEGRAM_SUPPORT_BOT_TOKEN: process.env.TELEGRAM_SUPPORT_BOT_TOKEN,
+        TELEGRAM_LOGGER_BOT_TOKEN: process.env.TELEGRAM_LOGGER_BOT_TOKEN,
+      },
+    },
+  ],
+};
