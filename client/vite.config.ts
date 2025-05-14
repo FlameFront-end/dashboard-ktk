a3a644dbe836f317ea9b1e2d2b64f674c2eeda33
FlameFront-end <5017_30@mail.ru>
2025-05-14 20:12:43 +0300

    fix: build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export const pathResolver = (p: string) => path.resolve(__dirname, '.', p);
 
 export default defineConfig({
-  base: './',
+  base: '/',
   publicDir: 'public',
   resolve: {
     alias: {
