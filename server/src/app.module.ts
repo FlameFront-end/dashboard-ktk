f8992b5ab74589260bafbbfc98e88dcb4a9f8a00
FlameFront-end <5017_30@mail.ru>
2025-05-17 20:40:55 +0300

    refactor: app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,34 +2,51 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { JwtModule } from '@nestjs/jwt'
+
 import { AuthModule } from './auth/auth.module'
 import { MailModule } from './mail/mail.module'
 import { TeachersModule } from './teachers/teachers.module'
-import { TeacherEntity } from './teachers/entities/teacher.entity'
 import { StudentsModule } from './students/students.module'
-import { StudentEntity } from './students/entities/student.entity'
 import { GroupsModule } from './groups/groups.module'
-import { GroupEntity } from './groups/entities/group.entity'
-import { ScheduleEntity } from './groups/entities/schedule.entity'
 import { AdminsModule } from './admins/admins.module'
-import { AdminEntity } from './admins/entities/admin.entity'
 import { DisciplinesModule } from './disciplines/disciplines.module'
-import { DisciplineEntity } from './disciplines/entities/discipline.entity'
-import { GradeEntity } from './groups/entities/grade.entity'
 import { LessonsModule } from './lessons/lessons.module'
-import { LessonEntity } from './lessons/entities/lesson.entity'
 import { ChatModule } from './chat/chat.module'
 import { MessagesModule } from './messages/messages.module'
+import { SupportModule } from './support-ticket/support.module'
+import { TelegramModule } from './telegram/telegram.module'
+
+import { TeacherEntity } from './teachers/entities/teacher.entity'
+import { StudentEntity } from './students/entities/student.entity'
+import { GroupEntity } from './groups/entities/group.entity'
+import { ScheduleEntity } from './groups/entities/schedule.entity'
+import { GradeEntity } from './groups/entities/grade.entity'
+import { AdminEntity } from './admins/entities/admin.entity'
+import { DisciplineEntity } from './disciplines/entities/discipline.entity'
+import { LessonEntity } from './lessons/entities/lesson.entity'
 import { MessageEntity } from './messages/entities/message.entity'
 import { ChatEntity } from './chat/entities/chat.entity'
-import { SupportModule } from './support-ticket/support.module'
 import { SupportTicketEntity } from './support-ticket/entities/support-ticket.entity'
-import { TelegramModule } from './telegram/telegram.module'
+
+const ENTITIES = [
+	TeacherEntity,
+	StudentEntity,
+	GroupEntity,
+	ScheduleEntity,
+	GradeEntity,
+	AdminEntity,
+	DisciplineEntity,
+	LessonEntity,
+	MessageEntity,
+	ChatEntity,
+	SupportTicketEntity
+]
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
-			envFilePath: `.env.${process.env.NODE_ENV}`
+			envFilePath: `.env.${process.env.NODE_ENV}`,
+			isGlobal: true
 		}),
 		TypeOrmModule.forRoot({
 			type: 'postgres',
@@ -38,22 +55,11 @@ import { TelegramModule } from './telegram/telegram.module'
 			username: process.env.POSTGRES_USER,
 			password: process.env.POSTGRES_PASSWORD,
 			database: process.env.POSTGRES_DB,
-			entities: [
-				TeacherEntity,
-				StudentEntity,
-				GroupEntity,
-				ScheduleEntity,
-				AdminEntity,
-				DisciplineEntity,
-				GradeEntity,
-				LessonEntity,
-				MessageEntity,
-				ChatEntity,
-				SupportTicketEntity
-			],
+			entities: ENTITIES,
 			synchronize: true,
 			ssl: false
 		}),
+		TypeOrmModule.forFeature(ENTITIES),
 		JwtModule.registerAsync({
 			imports: [ConfigModule],
 			useFactory: (configService: ConfigService) => ({
@@ -62,19 +68,7 @@ import { TelegramModule } from './telegram/telegram.module'
 			}),
 			inject: [ConfigService]
 		}),
-		TypeOrmModule.forFeature([
-			TeacherEntity,
-			StudentEntity,
-			GroupEntity,
-			ScheduleEntity,
-			AdminEntity,
-			DisciplineEntity,
-			GradeEntity,
-			LessonEntity,
-			MessageEntity,
-			ChatEntity,
-			SupportTicketEntity
-		]),
+
 		AuthModule,
 		MailModule,
 		TeachersModule,
@@ -83,7 +77,6 @@ import { TelegramModule } from './telegram/telegram.module'
 		AdminsModule,
 		DisciplinesModule,
 		LessonsModule,
-		LessonEntity,
 		ChatModule,
 		MessagesModule,
 		SupportModule,
